9bba892b25c91da7ec558fa57fe4d7bbaa13550b
Phil Corbett <corbett.phil@gmail.com>
2015-02-14 15:21:54 -0500

    spacing and formatting

--- JS/Engine/debug.js
+++ JS/Engine/debug.js
@@ -3,20 +3,22 @@
 
   var that = {},
     _log = new Array();
-    
-    that.debugString = '';
-    that.WriteOutput = function (inOutput) {
-    	if(inOutput) {
-	    	that.debugString = inOutput;
-	    }
-    };
-    
-    that.Log = function (inOutput) {
-    	_log.push(inOutput);
-    };
-	
-	that.getLog = function () {
-		return _log;
-	}
-    return that;
+
+  that.debugString = '';
+
+  that.WriteOutput = function (inOutput) {
+    if (inOutput) {
+      that.debugString = inOutput;
+    }
+  };
+
+  that.Log = function (inOutput) {
+    _log.push(inOutput);
+  };
+
+  that.getLog = function () {
+    return _log;
+  }
+
+  return that;
 })();
