82927697a74c91e3010263f47aaf3635b7b472cb
Phil Corbett <corbett.phil@gmail.com>
2015-02-14 15:17:49 -0500

    core unit tests

---
+++ test/spec/Engine/Core.js
@@ -0,0 +1,16 @@
+﻿describe('Engine.Core', function () {
+  var core;
+
+  beforeEach(function () {
+    core = TG.Engine.Core;
+  });
+
+  it('has Init', function () {
+    expect(core.Init).toBeDefined();
+  });
+
+  it('has AddTick', function () {
+    expect(core.AddTick).toBeDefined();
+  });
+
+});
