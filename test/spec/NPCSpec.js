cccfed5d43913f1ab60e14cf81b62c876cd211e4
Phil Corbett <corbett.phil@gmail.com>
2015-02-14 11:33:07 -0500

    more unit tests

--- test/spec/NPCSpec.js
+++ test/spec/NPCSpec.js
@@ -1,20 +1,23 @@
 ﻿describe('NPC', function () {
   var NPC;
+
   beforeEach(function () {
     NPC = new TG.Engines.Generate.NPC('Test', { x: 0, y: 0 }, 1);
   });
 
-  it('works', function () {
-    expect(1).toBe(1);
-  });
+  describe('General', function () {
+    it('works', function () {
+      expect(1).toBe(1);
+    });
 
-  it('has a name', function () {
-    expect(NPC.title).toBe('Test');
-  });
+    it('has a name', function () {
+      expect(NPC.title).toBe('Test');
+    });
 
-  it('has a position', function () {
-    expect(NPC.getPosition().x).toBe(0);
-    expect(NPC.getPosition().y).toBe(0);
+    it('has a position', function () {
+      expect(NPC.getPosition().x).toBe(0);
+      expect(NPC.getPosition().y).toBe(0);
+    });
   });
 
   describe('getProperties', function () {
@@ -27,5 +30,33 @@
     it('has a getState function', function () {
       expect(NPC.getState).toBeDefined();
     });
+
+    it('gets a state object', function () {
+      expect(NPC.getState()).toBeDefined();
+    });
+
+    it('has AI', function () {
+      expect(NPC.getState().AI).toBeDefined();
+    });
+
+    it('has Core', function () {
+      expect(NPC.getState().Core).toBeDefined();
+    });
+
+    it('has Environment', function () {
+      expect(NPC.getState().Environment).toBeDefined();
+    });
+
+    it('has TickCount', function () {
+      expect(NPC.getState().TickCount).toBeDefined();
+      expect(NPC.getState().TickCount).toBe(0);
+    });
+
+    describe('Combat', function () {
+      it('has Combat', function () {
+        expect(NPC.getState().Combat).toBeDefined();
+      });
+    });
   });
+
 });
